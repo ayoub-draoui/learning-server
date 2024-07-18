425387fa043a0b59fdcfecd9df08350a0a867432
ayoub-draoui <mr.stefan.draoui@gmail.com>
2024-07-18 11:04:33 +0100

    update01

--- server.go
+++ server.go
@@ -5,26 +5,26 @@ import (
 	"log"
 	"net/http"
 )
+	func helloHandler(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/hello" {
+			http.Error(w, "error 404", http.StatusNotFound)
+			return
+		}
+		if r.Method != "GET" {
+			http.Error(w, "Method Not Supported", http.StatusNotFound)
+			return
+		}
+		fmt.Fprintf(w, "hello mother fuckers")
+	}
+	func formHandler(w http.ResponseWriter, r http.Request) 
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+	 
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("starting port localhost:8080\n")
+	fmt.Printf("starting server at http://localhost:8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
-		return
 	}
+}
 
-}
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-	fmt.Fprintf(w, "hello mutherFuckers!!")
-}
