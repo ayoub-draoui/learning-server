44518e5b1094305c8205ba2728b131fe19684b07
ayoub-draoui <mr.stefan.draoui@gmail.com>
2024-07-20 10:40:14 +0100

    update02

--- server.go
+++ server.go
@@ -5,26 +5,39 @@ import (
 	"log"
 	"net/http"
 )
-	func helloHandler(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/hello" {
-			http.Error(w, "error 404", http.StatusNotFound)
-			return
-		}
-		if r.Method != "GET" {
-			http.Error(w, "Method Not Supported", http.StatusNotFound)
-			return
-		}
-		fmt.Fprintf(w, "hello mother fuckers")
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err: %v", err)
+		return
 	}
-	func formHandler(w http.ResponseWriter, r http.Request) 
+	fmt.Fprint(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "name =%s\n", name)
+	fmt.Fprint(w, "address =%s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "error 404", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello mother fuckers")
+}
 
 func main() {
-	 
+	fileServer :=  http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("starting server at http://localhost:8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
-
